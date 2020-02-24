c2ebb2986f0b07b01b831daf544bfc82a41bcb5c
昌宇 黄 <h1746672535@163.com>
2020-02-25 00:17:55 +0800

    。。。

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a=0;
+            double b=0;
+            double c = 0;
+            string d = comboBox1.Text;
+            try
+            {
+                if (!Double.TryParse(textBox1.Text, out a) || !Double.TryParse(textBox2.Text, out b))
+                {
+                    throw new ArgumentException();
+                }
+                if (a < Double.MinValue && a > Double.MaxValue && b < Double.MinValue && b > Double.MaxValue)
+                {
+                    throw new ArithmeticException();
+                }
+                switch (d)
+                {
+                    case "+":
+                        c = a + b;
+                        break;
+                    case "-":
+                        c = a - b;
+                        break;
+                    case "*":
+                        c = a * b;
+                        break;
+                    case "/":
+                        if (b != 0)
+                            c = a / b;
+                        else
+                            throw new DivideByZeroException();
+                        break;
+                }
+                textBox3.Text = c + "";
+                return;
+
+            }
+            catch (ArgumentException)
+            {
+                label2.Text = "请输入数字";
+                return;
+            }
+            catch (DivideByZeroException)
+            {
+                label2.Text = "除数不能为0";
+                return;
+            }
+            catch (ArithmeticException)
+            {
+                label2.Text = "计算发生溢出";
+                return;
+            }
+            
+         
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            comboBox1.Text = null;
+            label2.Text = "tips";
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    using System;
+
+    namespace Calculator
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+               
+                int num1 = 0; int num2 = 0;
+
+                Console.WriteLine("输入一个数字，然后按下“enter”");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                
+                Console.WriteLine("输入第二个数字,然后按下“enter”");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                
+                Console.WriteLine("选择一个运算符号");
+                Console.WriteLine("+");
+                Console.WriteLine("-");
+                Console.WriteLine("*");
+                Console.WriteLine("\\");
+                Console.Write("你的选择是？");
+
+               
+                switch (Console.ReadLine())
+                {
+                    case "a":
+                        Console.WriteLine($"你的结果是: {num1} + {num2} = " + (num1 + num2));
+                        break;
+                    case "s":
+                        Console.WriteLine($"你的结果是: {num1} - {num2} = " + (num1 - num2));
+                        break;
+                    case "m":
+                        Console.WriteLine($"你的结果是: {num1} * {num2} = " + (num1 * num2));
+                        break;
+                    case "d":
+                        Console.WriteLine($"你的结果是: {num1} / {num2} = " + (num1 / num2));
+                        break;
+                }
+                
+               
+            }
+        }
+    }
+}
