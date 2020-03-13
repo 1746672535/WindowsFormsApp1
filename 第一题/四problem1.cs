4543e7201dd06097361942e4767af18b5102af09
昌宇 黄 <h1746672535@163.com>
2020-03-13 11:30:56 +0800

    yes ok

---
+++ 第一题/四problem1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    namespace GenericApplication
+    {
+
+        // 链表节点
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+
+        //泛型链表类
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+
+            public GenericList()
+            {
+                tail = head = null;
+            }
+
+            public Node<T> Head
+            {
+                get => head;
+            }
+
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+            public void Foreach(Action<T> action)
+            {
+                for (Node<T> x = head; x != null; x = x.Next)
+                {
+                    action(x.Data);
+                }
+            }
+
+           
+        }
+
+        public class test
+        {
+            static void Main(string[] args)
+            {
+                // 整型List
+                GenericList<int> intlist = new GenericList<int>();
+                for (int x = 0; x < 10; x++)
+                {
+                    intlist.Add(x);
+                }
+                int sum = 0;
+                int min = int.MaxValue;
+                int max = int.MinValue;
+
+
+                intlist.Foreach(x => Console.WriteLine(x));
+                intlist.Foreach(x => sum += x);
+                intlist.Foreach(x => min = min > x ? x : min);
+                intlist.Foreach(x => max = max > x ? max : x);
+                Console.WriteLine(sum);
+                Console.WriteLine(min);
+                Console.WriteLine(max);
+
+
+
+
+            }
+        }
+
+    }
+}
---
+++ 第二题/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            //form1.clock1.StartClock(20);
+        }
+    }
+
+    public delegate void ClockHandler(object sender, ClockEventArgs args);
+    public class ClockEventArgs
+    { public int AlarmTime { set; get; } }
+
+    public class Clock
+    {
+        public event ClockHandler Tick;
+        public event ClockHandler Alarm;
+
+
+        public void StartClock(int alarmtime)
+        {
+            ClockEventArgs args = new ClockEventArgs() { AlarmTime = alarmtime };
+            int inittime = 0;
+
+            while (true)
+            {
+                Tick(this, args);
+
+                inittime++;
+                if (inittime == args.AlarmTime)
+                    Alarm(this, args);
+                    Thread.Sleep(1000);
+            }
+        }
+    }
+
+    public class Form
+    {
+        public Clock clock1 = new Clock();
+        public Form()
+        {
+            clock1.Tick += new ClockHandler(Clk_Tick);
+            clock1.Alarm += Clk_Alarm;
+        }
+        void Clk_Tick(object sender, ClockEventArgs args)
+        {
+            Console.WriteLine("Tick");
+        }
+        void Clk_Alarm(object sender, ClockEventArgs args)
+        {
+            Console.WriteLine("Alarm!!!");
+        }
+    }
+}
